a9b32fd317cf74ac741aa8d258c7bc45376ab66e
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2023-09-06 09:39:28 +0000

    deploy: 6d97449299de6d595d365cb61b46328075f6433b

---
+++ dist/tcp.d.ts
@@ -0,0 +1,42 @@
+/// <reference types="node" />
+import Net from 'net';
+interface iHostCb {
+    (data: Net.Socket | any): void;
+}
+interface ServerSocket extends Net.Socket {
+    authenticate: (token: string) => any | null; /** Nullable **/
+}
+interface ClientSocket extends Net.Socket {
+    authenticate: (token: string) => void;
+}
+export declare class NetServer {
+    host: string;
+    port: number;
+    alias: string;
+    server: Net.Server;
+    clients: Net.Socket[];
+    secret: string;
+    constructor({ host, port, secret }: {
+        host?: string;
+        port?: number;
+        secret?: string;
+    }, cb: (client: ServerSocket) => void);
+    verify: (token: any) => any;
+    create: (cb: iHostCb) => void;
+}
+export declare class NetClient {
+    client: any;
+    config: any;
+    last: number;
+    isRestarting: boolean;
+    callback: (client: ClientSocket) => void;
+    constructor({ host, port, token }: {
+        host?: string;
+        port?: number;
+        token?: string;
+    }, cb: (client: ClientSocket) => void);
+    restart: () => number;
+    start: () => void;
+}
+export {};
+//# sourceMappingURL=tcp.d.ts.map
---
+++ dist/tcp.js
@@ -0,0 +1,152 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NetClient = exports.NetServer = void 0;
+const net_1 = __importDefault(require("net"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const utils_1 = require("utils");
+class NetServer {
+    host;
+    port;
+    alias;
+    server;
+    clients = [];
+    secret;
+    constructor({ host, port, secret }, cb) {
+        this.host = host ?? '127.0.0.1';
+        this.port = port ?? 0;
+        this.secret = secret ?? utils_1.env.secret ?? 'secret';
+        this.alias = `TCP-Host ${this.host} ${this.port}`;
+        this.create(cb);
+    }
+    verify = (token) => {
+        try {
+            return jsonwebtoken_1.default.verify(token, this.secret);
+        }
+        catch (err) {
+            return null;
+        }
+    };
+    create = (cb) => {
+        try {
+            this.server = net_1.default.createServer();
+            this.server.listen(this.port, this.host, () => {
+                utils_1.log.success(`${this.alias}: Started!`);
+                this.server.on('connection', (client) => {
+                    const alias = `${client.remoteAddress}:${client.remotePort}`;
+                    utils_1.log.success(`${this.alias}: ${alias} / Connected!`);
+                    client.id = (0, utils_1.Uid)();
+                    client.isAuthenticated = false;
+                    client.decoded = null;
+                    this.clients.push(client);
+                    client.authenticate = (token) => {
+                        if (client.isAuthenticated) {
+                            return client.decoded;
+                        }
+                        else {
+                            const decoded = this.verify(token);
+                            if (decoded) {
+                                client.isAuthenticated = true;
+                                client.decoded = decoded;
+                                utils_1.log.success(`${this.alias}: Authorized!`);
+                                return decoded;
+                            }
+                            else {
+                                utils_1.log.warn(`${this.alias}: Authorization failed / ${token}`);
+                                client.write('Unauthorization failed!\r\n');
+                                (0, utils_1.Delay)(() => client.destroy(), 2500);
+                                return null;
+                            }
+                        }
+                    };
+                    client.on('close', (data) => {
+                        let index = this.clients.findIndex((o) => o.remoteAddress === client.remoteAddress && o.remotePort === client.remotePort);
+                        index !== -1 && this.clients.splice(index, 1);
+                        utils_1.log.warn(`${this.alias}: ${alias} / [idx_${index}] Disconnected!`);
+                    });
+                    cb(client);
+                });
+            });
+        }
+        catch (err) {
+            utils_1.log.error(`${this.alias}: While starting ${err.message}`);
+            this.port !== 0 && (0, utils_1.Delay)(() => this.create(cb), 15 * 1000);
+        }
+    };
+}
+exports.NetServer = NetServer;
+class NetClient {
+    client;
+    config;
+    last = Date.now();
+    isRestarting = false;
+    callback;
+    constructor({ host, port, token }, cb) {
+        this.config = {
+            host: host ?? '127.0.0.1',
+            port: port ?? 0,
+            token: token ?? '#',
+        };
+        this.callback = cb;
+        this.start();
+        (0, utils_1.Loop)(() => {
+            /** Consider ( as channel broken ) when server doesn't send message for over 30 (+~5) second **/
+            if (Date.now() - this.last > (30 * 1000)) {
+                this.last = Date.now();
+                this.restart();
+            }
+        }, 5 * 1000);
+    }
+    restart = () => {
+        if (this.isRestarting) {
+            return 0;
+        }
+        this.isRestarting = true;
+        try {
+            utils_1.log.warn(`NetClient: Removing current connections and listeners ...`);
+            this.client.removeAllListeners();
+            this.client.destroy();
+        }
+        catch (err) {
+            utils_1.log.error(`NetClient: While Removing current connections: ${err.message}`);
+        }
+        finally {
+            (0, utils_1.Delay)(() => {
+                this.isRestarting = false;
+                this.start();
+            }, 15 * 1000);
+        }
+    };
+    start = () => {
+        utils_1.log.success(`NetClient: Starting a new NET.SOCKET ...`);
+        this.client = new net_1.default.Socket();
+        this.client.authenticate = (token) => this.client.write(token);
+        this.client.connect(this.config, () => {
+            utils_1.log.success(`NetClient: Connection established with the server`);
+            this.callback(this.client);
+        });
+        /* this.client.on('data', (chunk: any) => {
+            try {
+                this.last = Date.now()
+                this.log(`Data received from the server [${chunk.toString().length}]`)
+                this.callback(chunk)
+            } catch (err) { }
+        }) */
+        this.client.on('error', (err) => {
+            utils_1.log.error(`NetClient: On.Error / ${err.message}`);
+            this.restart();
+        });
+        this.client.on('close', () => {
+            utils_1.log.warn(`NetClient: On.Close triggered!`);
+            this.restart();
+        });
+        this.client.on('end', () => {
+            utils_1.log.warn(`NetClient: On.End triggered!`);
+            this.restart();
+        });
+    };
+}
+exports.NetClient = NetClient;
+//# sourceMappingURL=tcp.js.map
