5a45043db39ab95efada1c26662e19f5252fa6a8
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2023-09-19 05:46:52 +0000

    deploy: 2aeb3ebeb885901a5ee730e987ecc2bdb11daaf6

---
+++ dist/connection.d.ts
@@ -0,0 +1,28 @@
+export declare class Connection {
+    private cio;
+    private caxios;
+    name: string;
+    token: string;
+    proxy: string;
+    timeout: number;
+    constructor(conf: {
+        name: string; /** host name **/
+        proxy?: string; /** proxy server **/
+        token?: string; /** bearer **/
+        timeout?: number; /** request timeout **/
+    });
+    connect: () => any;
+    exit: () => any;
+    /** _____________________________________________________________ HTTP-Client _____________________________________________________________ **/
+    res: (response: any) => any;
+    rej: (alias: string, error: any) => any;
+    get: (channel: any, data: any) => Promise<unknown>;
+    set: (channel: any, data: any) => Promise<unknown>;
+    pull: (channel: any, data: any, cb: any) => Promise<any>;
+    push: (channel: any, data: any, cb: any) => Promise<any>;
+    poll: (channel: any, data: any, cb: any) => void;
+    /** _____________________________________________________________ WS-Client _____________________________________________________________ **/
+    emit: (channel: any, data: any, cb: any) => void;
+    on: (channel: any, cb: any) => void;
+}
+//# sourceMappingURL=connection.d.ts.map
