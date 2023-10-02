4f912ed0b634f7b4477ab90552945f1f081310be
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2023-10-02 13:52:15 +0000

    deploy: 9dbea204215555cf5eb60ec10a32060802834d5a

---
+++ dist/replication.d.ts
@@ -0,0 +1,66 @@
+import { Host } from './host';
+import { Connection } from './connection';
+export declare class ReplicaMaster {
+    name: any;
+    table: any;
+    limit: any;
+    onChangeCall: any;
+    constructor({ me, name, table, channel, debug, limit, onPull, onTrigger, onSave, onChange }: {
+        me: string /** Device name */;
+        name: string /** Table name */;
+        table: any /** Sequel Table */;
+        channel: Host /** Host endpoint */;
+        debug?: boolean;
+        limit?: number /** Rows in a request */;
+        onPull?: () => {} | any /** Customize: Pull method */;
+        onTrigger?: () => {} /** Customize: That listens Sequel events and triggers replication */;
+        onSave?: () => {} | any /** Customize: Save method */;
+        onChange?: () => {} /** Customize: Change method */;
+    });
+    onPull: ({ id, dst, updatedAt }: {
+        id: any;
+        dst: any;
+        updatedAt: any;
+    }) => Promise<{
+        items: any;
+        checkpoint: any;
+    }>;
+    onTrigger: (next: any) => void;
+    onSave: (items: any) => Promise<string>;
+}
+export declare class ReplicaSlave {
+    lastPull: number;
+    isBusy: boolean;
+    hopes: any[];
+    success: number;
+    table: any;
+    name: any;
+    limit: number;
+    delay: number;
+    onChangeCall: any;
+    constructor({ me, name, table, channel, debug, retain, limit, delay, onPull, onPush, onTrigger, onSave, onChange }: {
+        me: string /** Device name */;
+        name: string /** Table name */;
+        table: any /** Sequel Table */;
+        channel: Connection /** Host endpoint */;
+        retain: [number | any, string | any] /** [5,'days'] -> Last 5 days of data will be replicated */;
+        debug?: boolean;
+        limit?: number /** Rows in a request */;
+        delay?: number /** Delay between request **/;
+        onPull?: () => {} /** Customize: Pull method */;
+        onPush?: () => {} /** Customize: Push method */;
+        onTrigger?: () => {} /** Customize: That listens Sequel events and triggers replication */;
+        onSave?: () => {} /** Customize: Save method */;
+        onChange?: () => {} /** Customize: Change method */;
+    });
+    onPull: (next: any) => void;
+    onPush: ({ id, src, dst, updatedAt }: {
+        id: any;
+        src: any;
+        dst: any;
+        updatedAt: any;
+    }, next: any) => void;
+    onTrigger: (next: any) => void;
+    onSave: (rows: any) => void;
+}
+//# sourceMappingURL=replication.d.ts.map
---
+++ dist/web/connection.js
@@ -0,0 +1,103 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Connection = void 0;
+const axios_1 = __importDefault(require("axios"));
+const web_1 = require("utils/web");
+const { io } = require("socket.io-client");
+const env = {};
+const whoami = env.whoami ?? "Browser";
+const proxy = env.proxy ?? window.location.origin;
+const token = env.token ?? "-";
+class Connection {
+    cio;
+    caxios;
+    name;
+    token;
+    proxy;
+    timeout;
+    constructor(conf) {
+        this.name = conf.name ?? '-';
+        this.token = conf.token ?? token;
+        this.proxy = conf.proxy ?? proxy;
+        this.timeout = conf.timeout ?? 5000;
+        web_1.log.success(`Creating connection: ${this.proxy}/${this.name}`);
+        this.cio = io(this.proxy, {
+            transports: ['websocket', 'polling'],
+            path: `/${this.name}/socket.io/`,
+            query: { host: this.name, whoami },
+            auth: { token: `Bearer ${this.token}` },
+        });
+        this.cio.on("connect", () => {
+            web_1.log.success(`ws:${this.name}: Connection made [${this.proxy}/${this.name}]`);
+            this.cio.sendBuffer = [];
+        });
+        this.cio.on('disconnect', () => {
+            web_1.log.warn(`ws:${this.name}: Disconnected [${this.proxy}/${this.name}]`);
+            (0, web_1.Delay)(() => this.cio.connect(), 2500);
+        });
+        this.cio.on("connect_error", (error) => {
+            console.log(error);
+            web_1.log.error(`ws:${this.name}: ${error.message}`);
+        });
+        this.caxios = axios_1.default.create({
+            baseURL: `${this.proxy}/${this.name}/`,
+            timeout: this.timeout,
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${this.token}`,
+                'whoami': whoami,
+            },
+            maxContentLength: Infinity,
+            maxBodyLength: Infinity,
+        });
+    }
+    connect = () => this.cio.connect();
+    exit = () => this.cio.disconnect();
+    /** _____________________________________________________________ HTTP-Client _____________________________________________________________ **/
+    get = (channel, data) => new Promise((resolve, reject) => {
+        this.caxios.get(channel, { params: data })
+            .then(({ data }) => resolve(data))
+            .catch(reject);
+    });
+    set = (channel, data) => new Promise((resolve, reject) => {
+        this.caxios.post(channel, data)
+            .then(({ data }) => resolve(data))
+            .catch(reject);
+    });
+    pull = (channel, data, cb) => {
+        if (typeof cb === 'undefined')
+            return this.get(channel, data);
+        else
+            return this.get(channel, data)
+                .then(response => cb(null, response))
+                .catch(err => cb(err, null));
+    };
+    push = (channel, data, cb) => {
+        if (typeof cb === 'undefined')
+            return this.set(channel, data);
+        else
+            return this.set(channel, data)
+                .then(response => cb(null, response))
+                .catch(err => cb(err, null));
+    };
+    /** _____________________________________________________________ HTTP/WS-Client _____________________________________________________________ **/
+    poll = (channel, data, cb) => {
+        const since = new web_1.Since(1000);
+        const update = () => this.pull(channel, data, cb);
+        since.call(() => update());
+        this.cio.on(channel, (go) => go && since.add());
+        update();
+    };
+    /** _____________________________________________________________ WS-Client _____________________________________________________________ **/
+    emit = (channel, data, volatile = true) => {
+        volatile ? this.cio.volatile.emit(channel, data) : this.cio.emit(channel, data);
+    };
+    on = (channel, cb) => {
+        this.cio.on(channel, cb);
+    };
+}
+exports.Connection = Connection;
+//# sourceMappingURL=connection.js.map
