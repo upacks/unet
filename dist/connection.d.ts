922eba912f3fde67a628300b3bb67f9e60325d3a
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2023-10-07 02:14:30 +0000

    deploy: 002545d813258fe4a7a48e61f9a36e3411b0a8b9

---
+++ dist/connection.d.ts
@@ -0,0 +1,29 @@
+export declare class Connection {
+    cio: any;
+    caxios: any;
+    name: string;
+    token: string;
+    proxy: string;
+    timeout: number;
+    rejectUnauthorized: boolean;
+    constructor(conf: {
+        name: string; /** host name **/
+        proxy?: string; /** proxy server **/
+        token?: string; /** bearer **/
+        timeout?: number; /** request timeout **/
+        rejectUnauthorized?: boolean; /** Ignore: Local issuer certificate **/
+    });
+    connect: () => any;
+    exit: () => any;
+    /** _____________________________________________________________ HTTP-Client _____________________________________________________________ **/
+    get: (channel: any, data: any) => Promise<unknown>;
+    set: (channel: any, data: any) => Promise<unknown>;
+    pull: (channel: any, data: any, cb: any) => Promise<any>;
+    push: (channel: any, data: any, cb: any) => Promise<any>;
+    /** _____________________________________________________________ HTTP/WS-Client _____________________________________________________________ **/
+    poll: (channel: any, data: any, cb: any) => void;
+    /** _____________________________________________________________ WS-Client _____________________________________________________________ **/
+    emit: (channel: any, data: any, volatile?: boolean) => void;
+    on: (channel: any, cb: any) => void;
+}
+//# sourceMappingURL=connection.d.ts.map
---
+++ dist/util.d.ts
@@ -0,0 +1,3 @@
+export declare const isAsync: (p: any) => boolean;
+export declare const execute: (f: any, req: any, res: any, content: any) => Promise<unknown>;
+//# sourceMappingURL=util.d.ts.map
