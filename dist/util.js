1fb7ec02599d6d24d7ac632aa9bc0e6e9a82a33d
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2023-11-17 03:12:18 +0000

    deploy: 5b607acf881c4da627396873e6ee9947c3f61f5b

---
+++ dist/util.js
@@ -0,0 +1,56 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.authenticate = exports.execute = exports.isAsync = void 0;
+const isAsync = (p) => p && (p.constructor.name === "AsyncFunction" || (typeof p === 'object' && typeof p.then === 'function'));
+exports.isAsync = isAsync;
+const execute = (f, req, res, content) => new Promise((resolve, reject) => {
+    if (typeof f === 'object') {
+        reject({ message: `Wrong` });
+    }
+    else {
+        if ((0, exports.isAsync)(f)) {
+            f(req, res, content).then((e) => {
+                resolve(e);
+            }).catch(e => {
+                reject(e);
+            });
+        }
+        else {
+            resolve(f(req, res, content));
+        }
+    }
+});
+exports.execute = execute;
+const authenticate = (req) => {
+    if ('headers' in req) {
+        const { verified, role } = req.headers;
+        if (typeof verified === 'string' && verified === 'yes') {
+            const roles = ['level-1', 'level-2', 'level-3', 'level-4', 'level-5'];
+            if (typeof role === 'string' && roles.includes(role)) {
+                try {
+                    const { project, name } = req.headers;
+                    return {
+                        proj: project,
+                        user: name,
+                        level: roles.findIndex((s) => s === role) + 1,
+                    };
+                }
+                catch { }
+            }
+            else {
+                try {
+                    const { project, name } = req.headers;
+                    return {
+                        proj: project,
+                        user: name,
+                        level: 0,
+                    };
+                }
+                catch { }
+            }
+        }
+    }
+    return null;
+};
+exports.authenticate = authenticate;
+//# sourceMappingURL=util.js.map
