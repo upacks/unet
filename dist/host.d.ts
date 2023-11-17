032b496713358a933f446a67a2e882eda60f0425
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2023-11-17 02:29:36 +0000

    deploy: 3956268c1bfa81f68ef400166dc05f9d26778d72

---
+++ dist/host.d.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+export declare class Host {
+    server: any;
+    io: any;
+    app: any;
+    requests: any;
+    name: string;
+    timeout: number;
+    port: number;
+    redis: boolean;
+    constructor(conf: {
+        name: string; /** name alias **/
+        port?: number;
+        static?: string; /** if serves static **/
+        timeout?: number; /** request timeout **/
+        redis?: boolean; /** use redis **/
+    });
+    emit: (channel: string, data: any) => void;
+    _on: (channel: string, callback: (req: Request, res: Response) => void, authorize?: boolean) => void;
+    on: (channel: string, callback: (req: Request, res: Response) => void, authorize?: boolean) => void;
+    exit: () => void;
+}
+//# sourceMappingURL=host.d.ts.map
