e17af172cff3f9cde7ea585bfd4695cbed3c5481
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2023-11-24 05:50:38 +0000

    deploy: 2c0103ddc83f43d6875e97206deaa5807857d029

---
+++ dist/util.d.ts
@@ -0,0 +1,8 @@
+export declare const isAsync: (p: any) => boolean;
+export declare const execute: (f: any, req: any, res: any, content: any) => Promise<unknown>;
+export declare const authenticate: (req: any) => {
+    proj: any;
+    user: any;
+    level: number;
+};
+//# sourceMappingURL=util.d.ts.map
