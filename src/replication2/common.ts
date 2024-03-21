1b3471d71c8d3ca72cdf4bf30411cf507a497165
gantulgak <gantulgak@riotinto.com>
2024-03-21 08:18:51 +0800

    v1.0.64

--- src/replication2/common.ts
+++ src/replication2/common.ts
@@ -2,7 +2,7 @@ import { pack, unpack } from 'msgpackr'
 import { zlibSync, unzlibSync } from 'fflate'
 import { log, Sfy } from 'utils'
 
-export const zip = (data, silent = true) => {
+export const zip = (data, logs = null) => {
 
     try {
 
@@ -10,20 +10,24 @@ export const zip = (data, silent = true) => {
         const bin = pack(data)
         const zip = zlibSync(bin, { level: 9 })
 
-        !silent && log.info(`String(size): ${typeof data} ` + actualSize)
-        !silent && log.info(`Pack(size): ${bin.constructor.name} ` + bin.length)
-        !silent && log.info(`Zip(size): ${zip.constructor.name} ` + zip.length)
-
-        console.log(`[#] Pack:       [${actualSize} -> ${zip.length}] ${(100 - ((zip.length * 100) / actualSize)).toFixed(1)}% eco`)
+        if (logs) logs.push(`[#] Pack:       [${actualSize} -> ${zip.length}] ${(100 - ((zip.length * 100) / actualSize)).toFixed(1)}% eco`)
+        else console.log(`[#] Pack:       [${actualSize} -> ${zip.length}] ${(100 - ((zip.length * 100) / actualSize)).toFixed(1)}% eco`)
 
         return zip
 
-    } catch (err) { throw new Error(`ZIP: ${err.message}`) }
+    } catch (err) {
+
+        if (logs) logs.push(`[#] Pack:       ${err.message}`)
+        else console.log(`[#] Pack:       ${err.message}`)
+
+        throw new Error(`ZIP: ${err.message}`)
+
+    }
 
 }
 
 
-export const unzip = (zip) => {
+export const unzip = (zip, logs = null) => {
 
     try {
 
@@ -31,10 +35,18 @@ export const unzip = (zip) => {
         const data = unpack(bin)
         const actualSize = typeof data === 'object' ? Sfy(data).length : data.length
 
-        console.log(`[#] Unpack:     [${zip.length} -> ${actualSize}] ${(100 - ((zip.length * 100) / actualSize)).toFixed(1)}% eco`)
+        if (logs) logs.push(`[#] Unpack:     [${zip.length} -> ${actualSize}] ${(100 - ((zip.length * 100) / actualSize)).toFixed(1)}% eco`)
+        else console.log(`[#] Unpack:     [${zip.length} -> ${actualSize}] ${(100 - ((zip.length * 100) / actualSize)).toFixed(1)}% eco`)
 
         return data
 
-    } catch (err) { throw new Error(`UNZIP: ${err.message}`) }
+    } catch (err) {
+
+        if (logs) logs.push(`[#] Unpack:     ${err.message}`)
+        else console.log(`[#] Unpack:     ${err.message}`)
+
+        throw new Error(`UNZIP: ${err.message}`)
+
+    }
 
 }
