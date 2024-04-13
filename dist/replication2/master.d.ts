b36e48397a432f94eb5ada318de61b7d8a6c68e7
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2024-04-13 02:49:43 +0000

    deploy: d1cb24cbfe6c04f51403adaa137a3eac6b4b017e

---
+++ dist/replication2/master.d.ts
@@ -0,0 +1,23 @@
+import { Host } from '../host';
+interface iRM {
+    sequel: any;
+    api: Host;
+    log?: boolean;
+    msgpackr?: boolean;
+    auth?: boolean;
+}
+export declare class rMaster {
+    _: iRM;
+    kv: any;
+    cb: any;
+    constructor(args: iRM);
+    /** Slave request: Items according to checkpoint */
+    get_items: (data: any, callback: any) => Promise<void>;
+    /** Slave request: Checkpoint of slave */
+    get_last: (data: any, callback: any) => Promise<void>;
+    /** Slave request: Sending items according to checkpoint */
+    send_items: (data: any, callback: any) => Promise<void>;
+    on_update: (cb: any) => any;
+}
+export {};
+//# sourceMappingURL=master.d.ts.map
